8d39535f54be607b7d2cf12d7f4c63b67200253b
Andronick Martusheff <amartush@starbucks.com>
2024-01-02 02:08:16 -0800

    Revert

--- components/TopBar/TopBar.js
+++ components/TopBar/TopBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import styles from './OCTopBar.styles';
+import styles from './TopBar.styles';
 import { Entypo } from '@expo/vector-icons';
 
 
