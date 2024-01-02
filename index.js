89af9f04f28b8facca78037d5081d3ce5bc12cf1
Andronick Martusheff <amartush@starbucks.com>
2024-01-02 10:03:11 -0800

    Remove React Native Navigation

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { registerRootComponent } from 'expo';
-import { enableScreens } from 'react-native-screens';
 
 import App from './App';
 
@@ -7,4 +6,3 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
-enableScreens();
